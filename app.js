90fc7aece92b8da190c4bb261a11eb6cb46e1f7e
catalinabrianese <catalinabrianese@hotmail.com>
2021-06-12 17:15:29 -0300

    modificamos carpeta ./public

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express= require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "./public")));
 
 
 app.listen(process.env.PORT || 3000, () => {
@@ -10,5 +10,5 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/index.html"));
+    res.sendFile(path.join(__dirname, "./views/index.html"));
 });
