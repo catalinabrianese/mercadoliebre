0d6b1a4b987f125288cfd36ef4a0dc91ebcda531
catalinabrianese <catalinabrianese@hotmail.com>
2021-06-12 16:59:44 -0300

    cambios en app y login.html

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const express= require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.join(__dirname, "/public")));
+
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
 
 app.get("/", function(req, res){
-    res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.sendFile(path.join(__dirname, "/views/index.html"));
 });
