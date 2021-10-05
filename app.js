00107abba1618083565ca0aff73f6bbbd8abcad2
catalinabrianese <catalinabrianese@hotmail.com>
2021-10-05 15:26:55 -0300

    modificaciones

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const path = require("path");
 app.use(express.static(path.join(__dirname, "./public")));
 
 
-app.listen(process.env.PORT || 3001, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+app.get("/login", function(req, res){
+    res.sendFile(path.join(__dirname, "./views/login.html"));
+});
+app.get("/register", function(req, res){
+    res.sendFile(path.join(__dirname, "./views/register.html"));
 });
